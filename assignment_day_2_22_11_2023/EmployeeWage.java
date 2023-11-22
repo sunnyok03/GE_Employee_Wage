a6562a1a82b086e685b2a2ea9c3e52652fdc45bb
sunnyok03 <sunnyok03@gmail.com>
2023-11-23 00:02:34 +0530

    wage calculator added

--- assignment_day_2_22_11_2023/EmployeeWage.java
+++ assignment_day_2_22_11_2023/EmployeeWage.java
@@ -4,11 +4,17 @@ public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage program.");
 
+        //attendance check
         int empChecker = (Math.random() <= 0.5) ? 0 : 1;// 0->absent and 1->present
         if(empChecker == 1) {
             System.out.println("Employee is Present");
         }else {
             System.out.println("Employee is not Present");
         }
+
+        //wage calculation
+        int wagePerHour = 20,fullDayHour = 8;
+        int fullTimeDailyWages = empChecker*wagePerHour*fullDayHour;
+        System.out.println("Daily wage of full time employee: "+fullTimeDailyWages);
     }
 }
