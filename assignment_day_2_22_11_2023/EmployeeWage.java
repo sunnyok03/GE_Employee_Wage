be846d6012b087a70ecb70f56ca54cc1e7c53033
sunnyok03 <sunnyok03@gmail.com>
2023-11-22 23:42:03 +0530

    UC1 attendance check

--- assignment_day_2_22_11_2023/EmployeeWage.java
+++ assignment_day_2_22_11_2023/EmployeeWage.java
@@ -3,5 +3,12 @@ package com.assignment_day_2_22_11_2023;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to employee wage program.");
+
+        int empChecker = (Math.random() <= 0.5) ? 0 : 1;// 0->absent and 1->present
+        if(empChecker == 1) {
+            System.out.println("Employee is Present");
+        }else {
+            System.out.println("Employee is not Present");
+        }
     }
 }
