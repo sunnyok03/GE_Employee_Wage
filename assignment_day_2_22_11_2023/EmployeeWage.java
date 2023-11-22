49d43e48c61d587a0a3c1c483066ab942b28e690
sunnyok03 <sunnyok03@gmail.com>
2023-11-22 23:36:27 +0530

    starting

---
+++ assignment_day_2_22_11_2023/EmployeeWage.java
@@ -0,0 +1,7 @@
+package com.assignment_day_2_22_11_2023;
+
+public class EmployeeWage {
+    public static void main(String[] args) {
+        System.out.println("Welcome to employee wage program.");
+    }
+}
